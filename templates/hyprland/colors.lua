6819134d8d7fdea65d2a5908f34ebe8c9ad942cf
Vitor Gabriel <37278974+zNyash@users.noreply.github.com>
2026-05-21 11:31:44 -0300

    yeah

---
+++ templates/hyprland/colors.lua
@@ -0,0 +1,16 @@
+hl.config({
+    general = {
+        col = {
+            active_border   = "rgb({{colors.primary.default.hex_stripped}})",
+            inactive_border = "rgb({{colors.tertiary_container.default.hex_stripped}})",
+        },
+    },
+    misc = {
+        background_color = "rgba({{colors.surface.dark.hex_stripped}}FF)",
+    },
+})
+
+hl.window_rule({
+    match        = { pin = 1 },
+    border_color = "rgba({{colors.primary.default.hex_stripped}}AA) rgba({{colors.primary.default.hex_stripped}}77)",
+})
