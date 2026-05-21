fc19ddcfca2e9f10d42cf3eb16716c8977f6a3de
Vitor Gabriel <37278974+zNyash@users.noreply.github.com>
2026-05-21 13:11:22 -0300

    feat(hyprland): add new binds for the apps

--- hypr/nsh/keybinds/apps.lua
+++ hypr/nsh/keybinds/apps.lua
@@ -1,10 +1,19 @@
--- hl.bind("SUPER + Q", hl.dsp.exec_cmd(terminal))     -- Termina
--- hl.bind("SUPER + E", hl.dsp.exec_cmd(fileManager))  -- File Manager
--- hl.bind("SUPER + D", hl.dsp.exec_cmd(Discord))      -- Discord
--- hl.bind("SUPER + B", hl.dsp.exec_cmd(browser))      -- Browser
+hl.unbind("SUPER + Q")
+hl.bind("SUPER + Q", hl.dsp.exec_cmd(Terminal)) -- Termina
+
+hl.unbind("SUPER + E")
+hl.bind("SUPER + E", hl.dsp.exec_cmd(FileManager)) -- File Manager
+
+hl.unbind("SUPER + D")
+hl.bind("SUPER + D", hl.dsp.exec_cmd(Discord)) -- Discord
+
+hl.unbind("SUPER + B")
+hl.bind("SUPER + B", hl.dsp.exec_cmd(Browser)) -- Browser
+
 -- hl.bind("SUPER + ALT + C", hl.dsp.exec_cmd("code")) -- Code
 -- hl.bind("SUPER + O", hl.dsp.exec_cmd())         -- OSU
--- hl.bind("SUPER + G", hl.dsp.exec_cmd())         -- Github Desktop
+hl.unbind("SUPER + G")
+hl.bind("SUPER + G", hl.dsp.exec_cmd(GithubDesktop)) -- Github Desktop
 
 -- hl.bind("SUPER + R", hl.dsp.exec_cmd("noctalia msg panel-toggle launcher"))           -- Launcher
 -- hl.bind("SUPER + L", hl.dsp.exec_cmd("noctalia msg panel-toggle session"))            -- Session
