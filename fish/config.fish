8881938e7fd54e7888780e0c414e314574a10358
Vitor Gabriel <37278974+zNyash@users.noreply.github.com>
2026-08-23 13:29:54 -0300

    fish: add comments and remove default greeting

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,25 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
+# source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
 
 # overwrite greeting
 # potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+function fish_greeting
+   # smth smth
+end
 
+# Aliases
+# Pipewire
 alias pw-config='code ~/.config/pipewire/pipewire-pulse.conf'
 alias pw-default-config='mkdir -p ~/.config/pipewire && cp -rv /usr/share/pipewire/* ~/.config/pipewire/ '
 alias pw-restart='systemctl --user restart pipewire pipewire-pulse'
 
+# Configs
 alias fish-config='code ~/.config/fish/config.fish'
 alias hypr-config='code ~/.config/hypr'
+
 # pnpm
 set -gx PNPM_HOME "/home/nyash/.local/share/pnpm"
 if not string match -q -- "$PNPM_HOME/bin" $PATH
