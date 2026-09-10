c41c7c7d74b4508955db1486ceaf239a96400870
Vitor Nyash <vitor.nyash@proton.me>
2026-09-10 16:23:41 -0300

    fish: add zoxide init command

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 # source /usr/share/cachyos-fish-config/cachyos-config.fish
-source ~/.config/fish/zoxide.fish
 source ~/.config/fish/cliamp.fish
+
+zoxide init --cmd cd fish | source
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
