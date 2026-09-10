046b6bf3416436eb33c1f47fe202cb54b2badc70
Vitor Nyash <vitor.nyash@proton.me>
2026-09-10 15:21:31 -0300

    nvim: use neo-tree and telescope as default

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        file_ignore_patterns = {},
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+      },
+    },
+  },
+}
